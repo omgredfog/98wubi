6041ff32ed819aff814fe451e824e675c5954eb1
omgredfog <35584974+omgredfog@users.noreply.github.com>
2019-01-24 16:20:01 +0800

    Delete rime.sh

--- rime.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash 
-rm -rf /usr/share/rime-data
-rm -rf ~/.config/fcitx/rime/*
-rm -rf ~/.config/ibus/rime/*
-mkdir /usr/share/rime-data
-cp -rf ./* /usr/share/rime-data/
-chmod -R 777 /usr/share/rime-data
-echo "中州韻配置文件安置成功，注銷系統後將重新部署。"
